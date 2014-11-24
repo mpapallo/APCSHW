24f5111827eb5b6cf5e758500dc2861244e7ec9f
Michaela Papallo <michaelapapallo@gmail.com>
2014-11-24 16:13:37 -0500

    separated addWord into wordFits and addWord, added addWordRandomly, put file scanning into driver

--- 03WordGrid/Driver.java
+++ 03WordGrid/Driver.java
@@ -4,9 +4,26 @@ public class Driver{
     public static void main(String[]args){
 	WordGrid z = new WordGrid(10, 10);
 	System.out.println(z);
-	
-	//z.createPuzzle();
-	//System.out.println(z);
+
+	try{
+	    
+	    File words = new File("words.txt");
+	    Scanner in = new Scanner(words);
+	    ArrayList<String> wordBank = new ArrayList<String>();
+	    Random r = new Random();
+
+	    while (in.hasNext()){
+		wordBank.add(in.next());
+	    }
+
+	    for (int x = 0; x < wordBank.size(); x ++){
+	       
+	    }
+	    System.out.println(wordBank);
+
+	}catch(FileNotFoundException e){
+	    System.out.println("File does not exist");
+	}
 	
     }
 }
--- 03WordGrid/WordGrid.java
+++ 03WordGrid/WordGrid.java
@@ -2,6 +2,7 @@ import java.util.*;
 import java.io.*;
 public class WordGrid{
     private char[][] data;
+    Random rand = new Random();
     
     /**
      *Initializes the grid to the size specified and fill all of
@@ -10,7 +11,11 @@ public class WordGrid{
      *@param cols is the starting width of the WordGrid.
      */
     public WordGrid(int rows, int cols){
-	data = new char[rows][cols];
+	if (rows == 0 && cols == 0){
+	    data = new char[10][10];
+	}else{
+	    data = new char[rows][cols];
+	}
 	clear();
     }
     public WordGrid(){
@@ -43,9 +48,35 @@ public class WordGrid{
 	    }
 	}
     }
-
-    //CHANGE '_' TO ' ' LATER!!!!!!!!!!!!!!!!!!!
     
+    /**
+     *Checks if the word fits at the specified position of the WordGrid and if it 
+     *has a corresponding letter to match any letters that it overlaps).
+     *@param word is the text to be added to the WordGrid.
+     *@param row is the vertical location of the beginning of the word.
+     *@param col is the horizontal location of the beginning of the word.
+     *@param x is an int from [-1, 1] that indicates the horizontal direction of the next letter in the word
+     *@param y is an int from [-1,1] that indicates the vertical direction of the next letter in the word
+     *@return true is the word fits, or false when the word
+     *doesn't fit or overlaps letters that don't match.
+     */
+
+    private boolean wordFits(String word, int row, int col, int x, int y){
+	if ((x == 0 && y == 0) ||
+	    row + y*word.length() > data.length ||
+	    col + x*word.length() > data[0].length){
+	    return false;
+	}
+	for (int n = 0; n < word.length(); n ++){
+	    if (data[row][col] != '_' && data[row][col] != word.charAt(n)){
+		return false;
+	    }
+	    row += y;
+	    col += x;
+	}
+	return true;
+    }
+
     /**
      *Attempts to add a given word to the specified position of the WordGrid.
      *The word is added from left to right, if it fits on the WordGrid and
@@ -58,95 +89,26 @@ public class WordGrid{
      *@return true when the word is added successfully, or false when the word
      *doesn't fit or overlaps letters that don't match.
      */
+
     public boolean addWord(String word, int row, int col, int x, int y){
-        int r = row;
-	int c = col;
-	for (int n = 0; n < word.length(); n ++){
-	    if (data[r][c] != '_' && data[r][c] != word.charAt(n)){
-		return false;
+        if (wordFits(word, row, col, x, y)){
+	    for (int n = 0; n < word.length(); n ++){
+		data[row][col] = word.charAt(n);
+		row += y;
+		col += x;
 	    }
-	    r += y;
-	    c += x;
+	    return true;
+	}else{
+	    return false;
 	}
-	r = row;
-	c= col;
-	for (int n = 0; n < word.length(); n ++){
-	    data[r][c] = word.charAt(n);
-	    r += y;
-	    c += x;
-	}
-	return true;
+    }
+
+    public boolean addWordRandomly(String word){
+	int row = rand.nextInt(data.length);
+	int col = rand.nextInt(data[0].length);
+        int x  = rand.nextInt(3) - 1;
+	int y = rand.nextInt(3) - 1;
+	return (addWord(word, row, col, x, y));
     }
    
-    File wordBank = new File("words.txt");
-    Scanner in = new Scanner(wordBank);
-    Random r = new Random();
-    ArrayList<String> words = new ArrayList<String>();
-    
-    public void createPuzzle(){
-	while(in.hasNext()){
-	    words.add(in.next());
-	}
-	for (int x = 0; x < words.size(); x ++){
-	    String word = words.get(x);
-	    int attempts = 0;
-	    while(attempts < 5){
-		int dir = r.nextInt(8);
-		int row = r.nextInt(data.length);
-		int col = r.nextInt(data[0].length);
-		if (dir == 0){
-		    if (addWord(word, row, col, 1, 0)){
-			attempts = 5;
-		    }else{
-			attempts += 1;
-		    }
-		}else if (dir == 1){
-		    if (addWord(word, row, col, 0, 1)){
-			attempts = 5;
-		    }else{
-			attempts += 1;
-		    }
-		}else if (dir == 2){
-		    if (addWord(word, row, col, 1, 1)){
-			attempts = 5;
-		    }else{
-			attempts += 1;
-		    }
-		}else if (dir == 3){
-		    if (addWord(word, row, col, 1, -1)){
-			attempts = 5;
-		    }else{
-			attempts += 1;
-		    }
-		}else if (dir == 4){
-		    if (addWord(word, row, col, -1, 0)){
-			attempts = 5;
-		    }else{
-			attempts += 1;
-		    }
-		}else if (dir == 5){
-		    if (addWord(word, row, col, 0, -1)){
-			attempts = 5;
-		    }else{
-			attempts += 1;
-		    }
-		}else if (dir == 6){
-		    if (addWord(word, row, col, 1, -1)){
-			attempts = 5;
-		    }else{
-			attempts += 1;
-		    }
-		}else{
-		    if (addWord(word, row, col, -1, -1)){
-			attempts = 5;
-		    }else{
-			attempts += 1;
-		    }
-		}
-	    }
-
-	   
-	}
-    }
-
 }
