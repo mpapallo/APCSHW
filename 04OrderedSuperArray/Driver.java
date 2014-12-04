d5476a62b3f065d68f7b31b53ce257bdbd0038d9
Michaela Papallo <michaelapapallo@gmail.com>
2014-12-03 20:46:53 -0500

    sort and beginning of documentation

--- 04OrderedSuperArray/Driver.java
+++ 04OrderedSuperArray/Driver.java
@@ -1,37 +1,49 @@
 public class Driver{
     public static void main(String[]args){
 
-	System.out.println("Ordered Super Array Stuff:\n\n");
-	OrderedSuperArray A = new OrderedSuperArray();
-	System.out.println(A);
-
+	/*
 	String a = "aardvark";
 	String b = "banana";
 	String c = "crawfish";
 	String d = "dugong";
 
+	System.out.println("\nOrdered Super Array Stuff:\n");
+	OrderedSuperArray A = new OrderedSuperArray();
+	System.out.println(A);
 	A.add(c);
 	System.out.println(A);
 	A.add(b);	
 	System.out.println(A);
-	
 	A.add(5, a);
 	System.out.println(A);
-
 	System.out.println(A.set(0, d));
 	System.out.println(A);
 
-
-	System.out.println("\nSuper Array Stuff:\n\n");
+	System.out.println("\nSuper Array Stuff:\n");
 	SuperArray B = new SuperArray(1);
 	B.add(d);
 	B.add(b);
 	B.add(c);
 	B.add(a);
+	B.add(b);
 	System.out.println(B);
-	
 	//B.sort();
 	B.insertionSort();
 	System.out.println(B);
+	*/
+
+	SuperArray C = new SuperArray();
+
+	for (int i = 25; i >= 0; i --){
+	    C.add("" + (char)('a' + i));
+	}
+	for (int i = 25; i >= 0; i --){
+	    C.add("" + (char)('A' + i));
+	}
+	System.out.println(C);
+	System.out.println();
+	C.insertionSort();
+	System.out.println(C);
+	
     }
 }
