f6d579973cc294fc36a9911b0ad5827eab6ff97e
Michaela Papallo <michaelapapallo@gmail.com>
2014-12-16 21:59:20 -0500

    adding mroe sorts to Sorts.java

--- 06Sorts/Sorts.java
+++ 06Sorts/Sorts.java
@@ -1,6 +1,6 @@
 public class Sorts{
     
-    public static void bubbleSort(int[] A){
+    public static void bubble(int[] A){
 	for (int i = 1 ; i < A.length; i ++){
 	    for (int x = 0; x < A.length - i; x ++){
 		if (A[x] > A[x+1]){
@@ -11,13 +11,27 @@ public class Sorts{
 	    }
 	}
     }
-    
+
+    public static void swap(int[]A){
+	if (A.length > 1){
+	    for (int i = 1; i < A.length; i ++){
+		int x = i;
+		while (x > 0 && A[x-1] > A[x]){
+		    int s = A[x];
+		    A[x] = A[x-1];
+		    A[x-1] = s;
+		    x --;
+		}
+	    }
+	}
+    }
+
     public static void main(String[]args){
 	int[] I = new int[10];
 	for (int i = 10; i > 0; i --){
 	    I[10 - i] = i;
 	}
-	bubbleSort(I);
+	bubble(I);
 	for(int i = 0; i < I.length; i ++){
 	    System.out.print(I[i] + " ");
 	}
