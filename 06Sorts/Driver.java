1e53e4faff7faf9c4eaf196e940de99230778203
Michaela Papallo <michaelapapallo@gmail.com>
2014-12-17 22:03:51 -0500

    added Driver

--- 06Sorts/Driver.java
+++ 06Sorts/Driver.java
@@ -1,12 +1,41 @@
+import java.util.Arrays;
 public class Driver{
     public static void main(String[]args){
-	int[] orig = new int[10];
+        long startTime, endTime;
+	int[] orig;
+
+        orig = new int[500000];
 	for (int i = orig.length; i > 0; i --){
 	    orig[orig.length - i] = i;
 	}
-	for (int i  = 0; i < orig.length; i ++){
-	    System.out.println(orig[i] + " ");
-	}
+
+	int[] copy1 = Arrays.copyOf(orig, orig.length);
+	System.out.println("Bubble:");
+	startTime = System.currentTimeMillis();
+	Sorts.bubble(copy1);
+	endTime = System.currentTimeMillis();
+	System.out.println(endTime - startTime);
+	
+        int[] copy2 = Arrays.copyOf(orig, orig.length);
+	System.out.println("Insertion:");
+	startTime = System.currentTimeMillis();
+	Sorts.insertion(copy2);
+	endTime = System.currentTimeMillis();
+	System.out.println(endTime - startTime);
+
+	int[] copy3 = Arrays.copyOf(orig, orig.length);
+	System.out.println("Selection:");
+	startTime = System.currentTimeMillis();
+	Sorts.selection(copy3);
+	endTime = System.currentTimeMillis();
+	System.out.println(endTime - startTime);
+
+	int[] copy4 = Arrays.copyOf(orig, orig.length);
+	System.out.println("Arrays.sort:");
+	startTime = System.currentTimeMillis();
+        Arrays.sort(copy4);
+	endTime = System.currentTimeMillis();
+	System.out.println(endTime - startTime);
 
     }
 }
